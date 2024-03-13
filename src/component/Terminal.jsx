a516a09c179a9e853a2437ba1a11815eca22f789
constayush <aayush@mail.com>
2024-03-13 22:20:12 +0530

    working on terminal

--- src/component/Terminal.jsx
+++ src/component/Terminal.jsx
@@ -2,25 +2,54 @@ import React from 'react'
 import Navbar from './navbar'
 
 function Terminal() {
+
+
+
+
+
+function handleInputChange(e){
+
+  console.log(e.target.value)
+
+}
+
+
+
+
+
+
+
+
+
+
   return (
     <div className='w-full h-screen center flex-col '>
-    
-
-
-<div className='termianl w-[80rem] h-[40rem] '>
-    <nav className='w-full h-[8%] bg-[#1b1b1b] flex items-center justify-between p-[2rem] '> <div className=''>logo</div> <div className=''>terminal</div> <div>logos</div></nav>
-    <div className='w-full h-[90%] bg-[#151515] rounded'>
-</div>
 
 
 
+      <div className='termianl w-[80rem] h-[40rem] '>
+        <nav className='w-full h-[8%] bg-[#1b1b1b] flex items-center justify-between p-[2rem] '> <div className=''>logo</div> <div className=''>terminal</div> <div>logos</div></nav>
+        <div className='w-full h-[90%] bg-[#151515] rounded text-white tfont'>
+
+
+<p> Welcome to termianl !</p>
 
 
 
-    </div>
-    
-    
-    
+
+         <input type='text' onChange={handleInputChange} className='w-full bg-transparent'></input>
+
+        </div>
+
+
+
+
+
+
+      </div>
+
+
+
     </div>
   )
 }
