a89af70999ec49528b3f0eed355aedaddc31b869
constayush <aayush@mail.com>
2024-04-07 22:37:05 +0530

    bugs fix  - on enter blank command output

--- src/component/Terminal.jsx
+++ src/component/Terminal.jsx
@@ -6,7 +6,7 @@ function Terminal() {
 
   let [prvInputsData, setPrvInputsData] = useState([]) // array to store all input data.
   let [inputData, setInputData] = useState()// to store current input data.
-  const commandInput = useRef(null) //input div ref
+  const input = useRef(null) //input div refSSSS
   const commands = ['whoami', 'projects', 'contact'] // array of commands
 
 
@@ -16,31 +16,49 @@ function Terminal() {
 
   function handleKeyPress(e) {
 
-    if (e.code == 'Enter' && commandInput.current.value ) {  // checking both condition if key is enter and input have some value in it. 
+    if (e.code == 'Enter' && input.current.value ) {  // checking both condition if key is enter and input have some value in it. 
 
 
 
         setPrvInputsData([...prvInputsData, inputData]); // updating array of input data
-        commandInput.current.value = null // setting input value to null
+        input.current.value = null // setting input value to null
 
 
 
     }
+
+else if(e.code == 'Enter') {
+
+  setPrvInputsData([...prvInputsData, ""]); // updating array of input data with empty value to show blank output
+  input.current.value = null // setting input value to null
+
+}
+
+
+
   }
 
 
   return (
     <div className='w-full h-screen center flex-col p-[1rem] '>
 
+
+
       <div className='termianl  w-full h-full    '>
 
-        <nav className='w-full h-[8%] text-white bg-[#1b1b1b] flex items-center justify-between p-[2rem] '>
+
+
+        <nav className='w-full  h-[8%] text-white bg-[#1b1b1b] flex items-center justify-between p-[2rem] '>
+
 
           <h1 className=" text-xl "><a href="/">As.</a></h1> <div className='text-xl '>terminal</div> <Link to='/'><div className='text-xl '>back</div></Link>
 
+
         </nav>
 
-        <div className='w-full p-[1rem] h-[90%] bg-[#151515] rounded text-white tfont overflow-auto'>
+    
+
+        <div className='w-full p-[1rem] h-[90%] overflow-auto bg-[#151515] rounded text-white tfont '>
 
 
           <p> Welcome to termianl !</p>
@@ -49,10 +67,15 @@ function Terminal() {
           {prvInputsData.map((data) => <div className='oldCommand  flex'><p>root@ayush:~#</p><p>{data}</p></div>)}
 
 
+
           <div className='newCommand flex'>
 
+
             <p>root@ayush:~#</p>
-            <input ref={commandInput} type='text' onKeyDown={handleKeyPress} onChange={handleInputChange} className='tinput w-full bg-transparent' />
+
+            <input ref={input} type='text' onKeyDown={handleKeyPress} onChange={handleInputChange} className='tinput w-full bg-transparent' />
+
+
 
           </div>
 
