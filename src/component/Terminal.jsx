8d2f7c7245e6d2b2923646d9780d8b79e2b29b8c
constayush <aayush@mail.com>
2024-04-13 00:25:31 +0530

    terminal commands added

--- src/component/Terminal.jsx
+++ src/component/Terminal.jsx
@@ -20,9 +20,9 @@ function Terminal() {
   let [inputData, setInputData] = useState()// to store current input data.
   const input = useRef(null) //input div refSSSS
   const commandsArr = ['whoami', 'projects', 'contact'] // array of commands
-const terminalContainer = useRef(null)
-let bgCounter = 0
-const bgImgArr = [orangeBg , whiteBg, blackBg, orangeDrak, multiBg]
+  const terminalContainer = useRef(null)
+  let bgCounter = 0
+  const bgImgArr = [orangeBg , whiteBg, blackBg, orangeDrak, multiBg]
 
 
   /* .. ...........................................ANIMATIONS..................................................*/
@@ -56,11 +56,17 @@ const bgImgArr = [orangeBg , whiteBg, blackBg, orangeDrak, multiBg]
 
         if (input.current.value == 'whoami') {
 
-          input.current.value = null // setting input value to null
+          input.current.value = null
+          setPrvInputsData((prvInputsData)=>[...prvInputsData, "whoami"]) 
+          setPrvInputsData((prvInputsData)=>[...prvInputsData, " Hey, I'm Ayush, an 18-year-old web developer fueled by curiosity and passionate about building ui interfaces. I'm all about turning ideas into digital reality and constantly seeking new challenges to tackle."])
 
+        }
+        if (input.current.value == 'help') {
 
-          //  setPrvInputsData([...prvInputsData, "whoami"])
-          //  setPrvInputsData((x)=>{console.log(x)})
+          input.current.value = null
+          setPrvInputsData((prvInputsData)=>[...prvInputsData, "help"]) 
+          setPrvInputsData((prvInputsData)=>[...prvInputsData, "whoami - introduction of me ,"])
+          
         }
 
       }
@@ -102,39 +108,53 @@ function backBtn(){
 
 
   return (
+
     <div ref={terminalContainer} className='terminal w-full lg:h-[100vh] h-[95vh] center flex-col p-[1rem] overflow-auto  rounded'>
-
-
 
 
       <div ref={terminal} className='  w-full h-full  bg-[#070707cf]  rounded-md'>
 
 
-
         <nav className='w-full  h-[8%] text-white bg-[#141414f3] flex items-center justify-between p-[2rem] rounded-md'>
 
-
-          <div className=" text-xl "><img src={themeIco} className='changeBgBtn  cursor-pointer w-8 h-8 rounded-full' onClick={changeBg} /></div> <div className='text-xl '>terminal</div> <Link to='/'><img src={closeIco} onClick={backBtn} className='cursor-pointer w-8 h-8 rounded-full '/></Link>
-
+          <div className=" text-xl ">
+          <img src={themeIco} className='changeBgBtn  cursor-pointer w-8 h-8 rounded-full' onClick={changeBg} />
+          </div> 
+          <div className='text-xl '>terminal</div> 
+          <Link to='/'><img src={closeIco} onClick={backBtn} className='cursor-pointer w-8 h-8 rounded-full '/></Link>
 
         </nav>
-
-
 
         <div className='w-full p-[1rem] h-[90%] overflow-auto   text-white tfont '>
 
 
-          <p> Welcome to termianl !</p>
+          <p> Welcome to my termianl portfolio!</p>
+          <pre >
+<br/>       __     ___    _  _____ _    _ 
+<br/>     /\\ \   / / |  | |/ ____| |  | |
+<br/>    /  \\ \_/ /| |  | | (___ | |__| |
+<br/>   / /\ \\   / | |  | |\___ \|  __  |
+<br/>  / ____ \| |  | |__| |____) | |  | |
+<br/> /_/    \_\_|   \____/|_____/|_|  |_|           
+         
+<br/>
+<p className='pt-[1rem]'>------------------------------------</p>
+<p className=''> for list of commands type <span className='greeting'>`help`</span>.</p>
+<p className='pb-[1rem]'>------------------------------------</p></pre> 
 
-          {prvInputsData.map((data) => <div className='oldCommand  flex'><p>root@ayush:~#</p><p>{data}</p></div>)}
 
 
-          {/* {commandOutputData.map((data) => <div className='oldCommand  flex'><p>root@ayush:~#</p><p>{data}</p></div>)} */}
+
+
+          {prvInputsData.map((data) => <div className='oldCommand  flex'><p>user@ayush:~#</p><p>{data}</p></div>)}
+
+
+   
 
           <div className='newCommand flex'>
 
 
-            <p>root@ayush:~#</p>
+            <p>user@ayush:~#</p>
 
             <input ref={input} type='text' onKeyDown={handleKeyPress} onChange={handleInputChange} className='tinput w-full bg-transparent' />
 
